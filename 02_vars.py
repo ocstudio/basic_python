3d1ec1424381148283ff68e7237a40fede1091bc
ocstudio Fedora <ayo100fuegos@mail.com>
2024-06-25 05:46:27 -0500

    como usar los input para interactuar con la CLI

---
+++ 02_vars.py
@@ -0,0 +1,18 @@
+print("hellow vars")
+
+#esto es una variable
+my_name = "Nicolas"
+print(my_name)
+#esto es una variable con numero
+my_age = 12
+print(my_age)
+
+my_name = "Santiago"
+print("aqui cambio",my_name)
+
+#input
+
+my_name = input("¿cual es tu nombre?")
+print("usando input", my_name)
+my_age = input("¿cual es tu edad?")
+               
