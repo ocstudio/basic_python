7adebc4ada3aefdb1d8256d2d6fb8839d493eea1
ocstudio Fedora <ayo100fuegos@mail.com>
2024-06-25 05:43:45 -0500

    operaciones basicas y el print

---
+++ 01_print.py
@@ -0,0 +1,25 @@
+print("hola, esto es el archivo 01_print.py")
+
+print("hola, soy nicolas y tengo 12 años")
+
+
+#operaciones matematicas 
+
+#suma
+
+print(12+5)
+
+#resta
+print(10-5)
+
+#multiplicacion
+print(2*3)
+
+#divicion
+print(8/2)
+
+#esto es un primer comentario de python en mis primeras clases aprendientdo a programar con python 
+
+""" las comillas son utilizadas para elaborar textos con varias lineas de codigo y explicar al grupo de programadores como se ejecutan los programas o notas sobre este """
+
+''' tambien se puede utilizar las comillas simples para elaborar comentarios de varias lineas'''
