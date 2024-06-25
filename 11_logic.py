2e14174db0ad3239b7bc884a6ad3c748ff10a674
ocstudio Fedora <ayo100fuegos@mail.com>
2024-06-25 05:40:34 -0500

    operadores logicos AND & OR

---
+++ 11_logic.py
@@ -0,0 +1,25 @@
+#and operador logico
+print('AND')
+print('True and True =>', True and True)
+print('True and False =>', True and False)
+print('False and True =>', False and True)
+print('False and False =>', False and False)
+
+print(10 > 5 and 5 < 10)
+print(10 > 5 and 5 > 10)
+
+print('-' * 15)
+'''ejemplo de stock para negocios con SKU y registro de inventarios la condicion en este negocio es que el stock debe ser mayor a 100 y menor a 1000'''
+stock = input('Ingrese el numero de stock => ')
+stock = int(stock)
+
+print(stock >= 100 and stock <= 1000)
+print('-' * 15)
+#or operador logico
+print('OR')
+print('True and True =>', True or True)
+print('True and False =>', True or False)
+print('False and True =>', False or True)
+print('False and False =>', False or False)
+
+
